5a5573248f9c7a7790b063e4d7668283e28a3ab5
Daniel Trostli <daniel.trostli@gmail.com>
2014-08-12 19:11:20 -0700

    Added JS file for google maps integration

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,93 @@
+sprig = {}
+
+var markersArray = []
+var counter = 0
+sprig.infoWindow = new google.maps.InfoWindow;
+
+sprig.makeMap = function() {
+  var mapOptions = {
+    center: new google.maps.LatLng(32.7150, -117.1625),
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  sprig.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+}
+
+sprig.getMarkers = function(e) {
+    e.preventDefault();
+    $.ajax({
+      url:"/categories",
+      type: "GET",
+      dataType: "json",
+      data: $('#category_form').serialize()
+    }).done(sprig.saveMarkers)
+}
+
+sprig.saveMarkers = function(markerData) {
+  sprig.clearMarkers();
+  sprig.markerData = markerData;
+  sprig.MakeMarkers(markerData);
+}
+
+sprig.MakeMarkers = function(markerData) {
+  var self = this
+  $.each(markerData, function(index, element) {
+    var contentInfo = '<p>' + '<strong>Title: </strong>' + String(element.title) + '</p>'
+    +  '<p>' + '<strong>Compensation: </strong>'  + String(element.compensation) + '</p>'
+    + '<p>' + '<strong>Description: </strong>'  + String(element.description) + '</p>'
+    + '<p>' + '<strong>Address: </strong>'  + String(element.address) + '</p>';
+
+    var marker = self.makeMarker(index, element)
+
+    google.maps.event.addListener(marker, 'mouseover', function() {
+      sprig.infoWindow.setOptions({disableAutoPan : false })
+      sprig.infoWindow.open(sprig.map, marker)
+      var content =  contentInfo;
+      sprig.infoWindow.setContent(content);
+    });
+    var temp = document.getElementById("map-canvas");
+    google.maps.event.addListener(sprig.map, 'click', function() {
+      sprig.infoWindow.close();
+    });
+  });
+}
+
+proMapper.makeMarker = function (index, markerData) {
+  global = markerData
+  var myLatlng = new google.maps.LatLng(markerData.latitude, markerData.longitude)
+  var num = index
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    title: 'Click to Zoom',
+    icon: 'https://chart.googleapis.com/chart?chst=d_map_pin_letter_withshadow&chld=|FF0000|000000'
+  });
+  marker.setMap(this.map);
+  markersArray.push(marker);
+
+  return marker
+}
+
+proMapper.clearMarkers = function() {
+  for (var i = 0; i < markersArray.length; i++) {
+    markersArray[i].setMap(null);
+  }
+  markersArray.length = 0;
+}
+
+
+
+function initializeMap() {
+    proMapper.makeMap();
+    $('#category_form').on('submit', proMapper.getMarkers);
+}
+
+$(document).ready(function(){
+  google.maps.event.addDomListener(window, "load", initializeMap);
+})
+
+
+
+
+
+
+
