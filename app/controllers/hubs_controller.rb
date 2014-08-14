14c3c31d59c6e5faabe5680e7a900880427715c3
Daniel Trostli <daniel.trostli@gmail.com>
2014-08-13 19:08:04 -0700

    Added scoping logic for dates orders across dates in time range

--- app/controllers/hubs_controller.rb
+++ app/controllers/hubs_controller.rb
@@ -3,8 +3,15 @@ class HubsController < ApplicationController
   def index
     @orders = Order.all
 
-    if params['hub']
-      @orders = Order.where(category_id: Hub.find_by(name: params['hub']).id)
+    if params['start_time'] && params['end_time']
+      # @orders = Order.where("created_at.hour >= :start_time AND created_at.hour <= :end_time", {start_time: params[:start_time], end_time: params[:end_time] } )
+      scoped_orders = []
+      @orders.each do |order|
+        if order.created_at.hour >= params['start_time'].to_i && order.created_at.hour < params['end_time'].to_i
+          scoped_orders << order
+        end
+      end
+      @orders = scoped_orders
     end
 
     respond_to do |format|
